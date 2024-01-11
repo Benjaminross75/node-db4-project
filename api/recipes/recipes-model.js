16f262a947edd97020bce98ba98aa4088708e18b
Ben Ross <mrbenross@gmail.com>
2024-01-11 11:15:30 -0600

    final-commit

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,9 +2,82 @@ const db = require('../../data/db-config')
 
 async function getRecipeById(recipe_id){
     const recipeRows = await db('recipes as r')
-    .where('recipe_id', recipe_id)
+    .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+    .leftJoin('step_ingredients as si', 'si.step_id', 's.step_id')
+    .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
+    .select(
+        'r.recipe_id',
+        'r.recipe_name',
+        's.step_id',
+        's.step_number',
+        's.step_text',
+        'si.ingredient_id',
+        'i.ingredient_name',
+        'si.quantity'
+    )
+    .orderBy('s.step_number')
+    .where('r.recipe_id', recipe_id)
 
-    return recipeRows
+    const recipes = {
+        recipe_id: recipeRows[0].recipe_id,
+        recipe_name:recipeRows[0].recipe_name,
+        steps: recipeRows.reduce((acc, row) =>{
+             if(!row.ingredient_id){
+                return acc.concat({
+                    step_id: row.step_id,
+                    step_number: row.step_number,
+                    step_text: row.step_text,
+
+                })
+             }
+
+             if(row.ingredient_id && !acc.find(step => step.step_id === row.step_id)){
+                return acc.concat({
+                    step_id: row.step_id,
+                    step_number: row.step_number,
+                    step_text: row.step_text,
+                    ingredients: [
+                        {
+                            ingredient_id: row.ingredient_id,
+                            ingredient_name: row.ingredient_name,
+                            quantity: row.quantity,
+                        }
+                    ]
+                })
+             }
+
+             const currentStep = acc.find(step => step.step_id === row.step_id)
+             currentStep.ingredients.push({
+                ingredient_id: row.ingredient_id,
+                ingredient_name: row.ingredient_name,
+                quantity: row.quantity,
+             })
+             return acc
+        },[])
+    }
+
+    // {
+    //     "recipe_id" : 1,
+    //     "recipe_name": "Spaghetti Bolognese",
+    //     "created_at": "2021-01-01 08:23:19.120",
+    //     "steps": [
+    //       {
+    //         "step_id": 11,
+    //         "step_number": 1,
+    //         "step_instructions": "Put a large saucepan on a medium heat",
+    //         "ingredients": []
+    //       },
+    //       {
+    //         "step_id": 12,
+    //         "step_number": 2,
+    //         "step_instructions": "Add 1 tbsp olive oil",
+    //         "ingredients": [
+    //           { "ingredient_id": 27, "ingredient_name": "olive oil", "quantity": 0.014 }
+    //         ]
+    //       },
+    //     ]
+    //   }
+    return recipes
 }
 
 
